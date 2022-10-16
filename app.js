1f47c139109a2dd0bdad41acb698f6a1baf594b2
mac-mac-mac <macfbarron@gmail.com>
2022-10-16 23:50:17 +0000

    Created todo.model.js and edtied todo.ejs and app.js

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-var express = require('express')
+var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
 app.use('/static', express.static("public"));
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 
-app.get('/hello', function(req, res){
-    res.send('Hello World');
-})
+const mongoDB = 'mongodb+srv://macmacmac:mongo999DB123pass@cluster0.pxepcpc.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongoDB)
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDb connection error: "))
 
 app.get('/', function(req, res){
     res.render('todo.ejs');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.content)
 })
 
 app.listen(3000, function(){
